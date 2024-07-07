38c847f056a1d4db5dd372999bb6520137450803
Lee Damil <lidamil6785@gmail.com>
2024-07-07 18:44:31 +0600

    activation button change from "/" to "space"

--- Random chess square generator.py
+++ Random chess square generator.py
@@ -8,20 +8,18 @@ letters = ["a", "b", "c", "d", "e", "f", "g", "h"]
 numbers = [1, 2, 3, 4, 5, 6, 7, 8]
 
 while True:
-    if keyboard.read_key() == "/":
+    if keyboard.read_key() == "space":
         square = f"{random.choice(letters)}{random.choice(numbers)}"
         os.system('cls')
         print()
         sleep(0.1)
         print(square)
+     
+        output = gtts.gTTS(text=square, lang="en", slow=False)
 
-        os.system("taskkill /im wmplayer.exe")
+        output.save("output.mp3") 
 
-        output = gtts(text=square, lang="en", slow=False)
-
-        output.save("output.mp3")
-
-        os.system("start output.mp3")
+        os.system("start output.mp3") 
 
 
 
