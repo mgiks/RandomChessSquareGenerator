320266fa9f7b2bf1f92851ffc30c5ba36ec27e28
Lee Damil <lidamil6785@gmail.com>
2024-07-07 17:50:42 +0600

    First commit

---
+++ Random chess square generator.py
@@ -0,0 +1,27 @@
+import random
+import keyboard
+import os
+import gtts
+from time import sleep
+
+letters = ["a", "b", "c", "d", "e", "f", "g", "h"]
+numbers = [1, 2, 3, 4, 5, 6, 7, 8]
+
+while True:
+    if keyboard.read_key() == "/":
+        square = f"{random.choice(letters)}{random.choice(numbers)}"
+        os.system('cls')
+        print()
+        sleep(0.1)
+        print(square)
+
+        os.system("taskkill /im wmplayer.exe")
+
+        output = gtts(text=square, lang="en", slow=False)
+
+        output.save("output.mp3")
+
+        os.system("start output.mp3")
+
+
+
